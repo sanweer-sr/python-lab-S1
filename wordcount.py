fda04e9b998735ef23e2007606508a0459ba88fe
sanweer-sr <sanweersr@gmail.com>
2025-09-15 13:54:37 +0530

    Add files via upload

---
+++ wordcount.py
@@ -0,0 +1,20 @@
+strings = input("Enter the senetnce to check occurence of word : ")
+
+splitstrings = strings.split()
+print(splitstrings)
+
+i=0
+count =0
+
+while (i<len(splitstrings)):
+    
+    j=0
+    while (j<len(splitstrings)):
+        if (splitstrings[i] == splitstrings[j]):
+            splitstrings.pop(j)
+            count = count+1
+        j=j+1
+    print(f"{splitstrings[i]} - {count}")
+    count =0
+    i=i+1
+    
