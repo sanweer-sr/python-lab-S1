d3cd0c20ce506b6a5048e47862e83a90bb29924a
sanweer-sr <sanweersr@gmail.com>
2025-10-06 16:29:10 +0530

    Add files via upload

---
+++ 06-10/circlearealambda.py
@@ -0,0 +1,9 @@
+radius = int(input("Enter the Radius of a circle"))
+
+area = lambda radius : ((22/7)*radius*radius)
+
+def circle_area(rad):
+    return ((22/7)*rad*rad)
+
+print(area(radius))
+print(circle_area(radius))
---
+++ 06-10/pyramid.py
@@ -0,0 +1,10 @@
+rows = int(input("Enter the number of rows: "))
+num = 1
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
+
+
